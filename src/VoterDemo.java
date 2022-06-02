f812521b1d52c5d50169ea16db5fffb5e2025ab1
raipramod <pramodchamlingrai@gmail.com>
2022-06-01 21:51:05 -0600

    code added

---
+++ src/VoterDemo.java
@@ -0,0 +1,11 @@
+public class VoterDemo {
+    public static void main(String[] args) {
+        int voterAge=18;
+        if(voterAge>=18) {
+            System.out.println("I am Voter");
+        }
+        else{
+            System.out.println("I am not voter");
+        }
+    }
+}
